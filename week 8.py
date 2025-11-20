862d8fd4643d1ea81d3fe148d1b16389b570ca0c
jo <JM2471@live.mdx.ac.uk>
2025-11-20 09:29:03 +0400

    week 8 file

--- week 8.py
+++ week 8.py
@@ -5,11 +5,23 @@ from pathlib import Path
 
 # Define paths
 DATA_DIR = Path("DATA")
-DB_PATH = DATA_DIR / "intelligence_platform.db"
+DB_PATH = DATA_DIR/"intelligence_platform.db"
 
 # Create DATA folder if it doesn't exist
 DATA_DIR.mkdir(parents=True, exist_ok=True)
 
+conn=sqlite3.connect('DATA/intelligence_platform.db')
+cursor=conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    role TEXT DEFAULT 'user')""")
+
+conn.commit()
+
 print(" Imports successful!")
 print(f" DATA folder: {DATA_DIR.resolve()}")
-print(f" Database will be created at: {DB_PATH.resolve()}")
+print(f" This will destroy the pc. Database will be created at: {DB_PATH.resolve()}")
